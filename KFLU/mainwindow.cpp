3a22d8d5f289a4b30de2ce4e524578bd9f790c15
HyoeunKim <hyonnnnn@gmail.com>
2016-10-25 15:13:13 +0900

    changed lungekutta

--- KFLU/mainwindow.cpp
+++ KFLU/mainwindow.cpp
@@ -5,6 +5,8 @@
 #include <define.h>
 #include <algorithm>  
 
+#include <sstream>
+
 #include <qstring.h>
 
 using namespace std;
@@ -44,6 +46,7 @@ double k4[OutputArray] = {};
 
 int GraphAge = Age0to6; //default
 bool AgeAll = true;
+
 
 QVector<double> SusceptibleArray(NumberofArray);
 QVector<double> ExposedArray(NumberofArray);
@@ -85,9 +88,6 @@ void MainWindow::run(double input) {
 
 	Initialize();
 	InitialY();
-
-	MainWindow::step();
-
 	while (day + h < input) {
 
 		MainWindow::step();
@@ -110,8 +110,6 @@ void MainWindow::step() {
 
 	// k1
 	Evaluation(day, yInVector, k1);
-	//double total = InitY[S(Age0to6, LowRisk)] + InitY[S(Age0to6, HighRisk)] + InitY[S(Age7to12, LowRisk)] + InitY[S(Age7to12, HighRisk)] + InitY[S(Age13to18, LowRisk)] + InitY[S(Age13to18, HighRisk)] + InitY[S(Age13to18, LowRisk)] + InitY[S(Age19to64, HighRisk)] + InitY[S(Age65toEnd, LowRisk)] + InitY[S(Age65toEnd, HighRisk)];
-
 	evals++;
 
 	// k2
@@ -125,6 +123,7 @@ void MainWindow::step() {
 	
 	Evaluation(day + 0.5 * h, yInVector, k3);
 	evals++;
+
 	// Calculate error
 	double normK = 0.0;
 	double max = 0.0;
@@ -157,51 +156,70 @@ void MainWindow::step() {
 	// Update x and y
 	day += h;
 	for (int i = 0; i < OutputArray; i++) InitY[i] += h*(k1[i] + 2 * (k2[i] + k3[i]) + k4[i]) / 6;
+
+	// Increase h, if error was very small
+	if (error < minError) h *= 2;
+	
+	SusceptibleArray[day] = 0;
 	ExposedArray[day] = 0;
 	AsymptomaticArray[day] = 0;
 	ModerateArray[day] = 0;
 	SevereArray[day]=0;
+	DeadArray[day] = 0;
+	ImmuneArray[day] = 0;
+
 	if (AgeAll) {
-		SusceptibleArray[day] = total*(InitY[S(Age0to6, LowRisk)] + InitY[S(Age0to6, HighRisk)]+ InitY[S(Age7to12, LowRisk)] + InitY[S(Age7to12, HighRisk)]+ InitY[S(Age13to18, LowRisk)] + InitY[S(Age13to18, HighRisk)] + InitY[S(Age19to64, LowRisk)] + InitY[S(Age19to64, HighRisk)] + InitY[S(Age65toEnd, LowRisk)] + InitY[S(Age65toEnd, HighRisk)]);
-		for (int k = 0; k < EstageGroups; k++)
-			ExposedArray[day] += total*(InitY[E(Age0to6, LowRisk,k)] + InitY[E(Age0to6, HighRisk, k)] + InitY[E(Age7to12, LowRisk, k)] + InitY[E(Age7to12, HighRisk, k)] + InitY[E(Age13to18, LowRisk, k)] + InitY[E(Age13to18, HighRisk, k)] + InitY[E(Age19to64, LowRisk, k)] + InitY[E(Age19to64, HighRisk, k)] + InitY[E(Age65toEnd, LowRisk, k)] + InitY[E(Age65toEnd, HighRisk, k)]);
-			
-		//ExposedArray[day] = total*ExposedArray[day];
-		for (int k = 0; k < IstageGroups; k++) {
-			AsymptomaticArray[day] += total*(InitY[A(Age0to6, k)]+ InitY[A(Age7to12, k)] + InitY[A(Age13to18, k)] + InitY[A(Age19to64, k)] + InitY[A(Age65toEnd, k)]);
-			ModerateArray[day] += total*( InitY[M(Age0to6, k)] + InitY[M(Age7to12, k)] + InitY[M(Age13to18, k)] + InitY[M(Age19to64, k)] + InitY[M(Age65toEnd, k)]);
-			SevereArray[day] += total*(InitY[V(Age0to6, k)] + InitY[V(Age7to12, k)] + InitY[V(Age13to18, k)] + InitY[V(Age19to64, k)] + InitY[V(Age65toEnd, k)]
-				+ InitY[X(Age0to6, k)] + InitY[X(Age7to12, k)] + InitY[X(Age13to18, k)] + InitY[X(Age19to64, k)] + InitY[X(Age65toEnd, k)]);
-			for (int m = 0; m < MedGroups; m++) {
-				SevereArray[day] += total*(InitY[W(Age0to6, k,m)] + InitY[W(Age7to12, k,m)] + InitY[W(Age13to18, k,m)] + InitY[W(Age19to64, k,m)] + InitY[W(Age65toEnd, k,m)]);
-				for (int c = 0; c< ICUGroups; c++) {
-					SevereArray[day] += total*(InitY[H(Age0to6, k, m,c)] + InitY[H(Age7to12, k, m,c)] + InitY[H(Age13to18, k, m,c)] + InitY[H(Age19to64, k, m,c)] + InitY[H(Age65toEnd, k, m,c)]);
+
+		for (int age = 0; age < StageofAgeGroups; age++) {
+			SusceptibleArray[day] += total*(InitY[S(age, LowRisk)] + InitY[S(age, HighRisk)]);
+
+			for (int k = 0; k < EstageGroups; k++) 
+				ExposedArray[day] += total*(InitY[E(age, LowRisk, k)] + InitY[E(age, HighRisk, k)]);
+
+
+			for (int k = 0; k < IstageGroups; k++) {
+				AsymptomaticArray[day] += total*(InitY[A(age, k)]);
+
+				ModerateArray[day] += total*(InitY[M(age, k)]);
+
+
+				SevereArray[day] += total*(InitY[V(age, k)] + InitY[X(age, k)]);
+
+
+				for (int m = 0; m < MedGroups; m++) {
+					SevereArray[day] += total*(InitY[W(age, k, m)]);
+
+						SevereArray[day] += total*(InitY[H(age, k, m)]);
+						//qDebug("day:%f, age:%d, istage:%d, med:%d, icu:%d %f", day, age, k,m,c, gamma[age][m][k][c]);
 				}
 			}
+
+			DeadArray[day] += total*(InitY[D(age)]);
+			ImmuneArray[day] += total*(InitY[I(age)]);
 		}
-		//W(Age, k, MedNO)
-		DeadArray[day] = total*(InitY[D(Age0to6)]+ InitY[D(Age7to12)]+ InitY[D(Age13to18)]+ InitY[D(Age19to64)]+ InitY[D(Age65toEnd)]);
-		ImmuneArray[day] = total*(InitY[I(Age0to6)] + InitY[I(Age7to12)] + InitY[I(Age13to18)] + InitY[I(Age19to64)] + InitY[I(Age65toEnd)]);
-		double helpme = SusceptibleArray[day] + ExposedArray[day] + AsymptomaticArray[day] + ModerateArray[day] + SevereArray[day] + DeadArray[day] + ImmuneArray[day];
-		qDebug("%f", helpme);
+
 	}
 	else {
-		SusceptibleArray[day] = InitY[S(GraphAge, LowRisk)] + InitY[S(GraphAge, HighRisk)];
+		SusceptibleArray[day] += total*(InitY[S(GraphAge, LowRisk)] + InitY[S(GraphAge, HighRisk)]);
 		for (int k = 0; k < EstageGroups; k++)
-			ExposedArray[day] += InitY[E(GraphAge, LowRisk, k)] + InitY[E(GraphAge, HighRisk, k)];
+			ExposedArray[day] += total*(InitY[E(GraphAge, LowRisk, k)] + InitY[E(GraphAge, HighRisk, k)]);
 
 		for (int k = 0; k < IstageGroups; k++) {
-			AsymptomaticArray[day] += InitY[A(GraphAge, k)];
-			ModerateArray[day] += InitY[M(GraphAge, k)];
-			SevereArray[day] += InitY[S(GraphAge, k)];
+			AsymptomaticArray[day] += total*(InitY[A(GraphAge, k)]);
+			ModerateArray[day] += total*(InitY[M(GraphAge, k)]);
+			SevereArray[day] += total*(InitY[V(GraphAge, k)] + InitY[X(GraphAge, k)]);
+
+			for (int m = 0; m < MedGroups; m++) {
+				SevereArray[day] += total*(InitY[W(GraphAge, k, m)]);
+
+					SevereArray[day] += total*(InitY[H(GraphAge, k, m)]);
+				
+			}
 		}
-		DeadArray[day] = InitY[D(GraphAge)];
-		ImmuneArray[day] = InitY[I(GraphAge)];
+		DeadArray[day] += InitY[D(GraphAge)];
+		ImmuneArray[day] += InitY[I(GraphAge)];
 	}
 
-
-	// Increase h, if error was very small
-	if (error < minError) h *= 2;
 }
 
 
@@ -305,27 +323,27 @@ void MainWindow::makeInfectionPlot(){
 	}
 
 	for (int i = 0; i < goal; i++) {
-		ui->infectionTable->setItem(i, 1, new QTableWidgetItem(QString::number(ExposedArray[i])));
+		ui->infectionTable->setItem(i, 1, new QTableWidgetItem(QString::number((double)ExposedArray[i], 'f')));
 	}
 
 	for (int i = 0; i < goal; i++) {
-		ui->infectionTable->setItem(i, 2, new QTableWidgetItem(QString::number(AsymptomaticArray[i])));
+		ui->infectionTable->setItem(i, 2, new QTableWidgetItem(QString::number((double)AsymptomaticArray[i], 'f')));
 	}
 
 	for (int i = 0; i <goal; i++) {
-		ui->infectionTable->setItem(i, 3, new QTableWidgetItem(QString::number(ModerateArray[i])));
+		ui->infectionTable->setItem(i, 3, new QTableWidgetItem(QString::number((double)ModerateArray[i], 'f')));
 	}
 
 	for (int i = 0; i <goal; i++) {
-		ui->infectionTable->setItem(i, 4, new QTableWidgetItem(QString::number(SevereArray[i])));
+		ui->infectionTable->setItem(i, 4, new QTableWidgetItem(QString::number((double)SevereArray[i], 'f')));
 	}
 
 	for (int i = 0; i < goal; i++) {
-		ui->infectionTable->setItem(i, 5, new QTableWidgetItem(QString::number(DeadArray[i])));
+		ui->infectionTable->setItem(i, 5, new QTableWidgetItem(QString::number((double)DeadArray[i], 'f')));
 	}
 
 	for (int i = 0; i < 200; i++) {
-		ui->infectionTable->setItem(i, 6, new QTableWidgetItem(QString::number(ImmuneArray[i])));
+		ui->infectionTable->setItem(i, 6, new QTableWidgetItem(QString::number((double)ImmuneArray[i], 'f')));
 	}
 }
 
@@ -1377,12 +1395,9 @@ void MainWindow::on_actionK_flu_triggered()
 
 }
 
+
 void MainWindow::on_actionExport_triggered()
 {
-	FILE* fp = fopen("output/test.csv", "w+");
-	fprintf(fp, "KFLU,\n");
-	fprintf(fp, "test,cell1,cell2\n");
-	fclose(fp);
 }
 
 void MainWindow::on_actionContact_triggered()
@@ -1409,7 +1424,24 @@ void MainWindow::on_jpgSubmit_clicked()
     ui->jpgWidget->hide();
 }
 
+const char* ConvertDoubleToString(double value) {
+	std::stringstream ss;
+	ss << value;
+	const char* str = ss.str().c_str();
+	return str;
+}
+
 void MainWindow::on_csvSubmit_clicked()
 {
+	
+	FILE* fp = fopen("output/result.csv", "w+");
+	fprintf(fp, "KFLU Output,\n");
+	fprintf(fp, "Susceptible,exposed,Asymptomatic,Moderate,Severe,dead,Immune,total\n");
+
+	for (int i = 0; i < 500; i++) {
+		double helpme = SusceptibleArray[i] + ExposedArray[i] + AsymptomaticArray[i] + ModerateArray[i] + SevereArray[i] + DeadArray[i] + ImmuneArray[i];
+		fprintf(fp, "%f,%f,%f,%f,%f,%f,%f,%f,\n",SusceptibleArray[i], ExposedArray[i], AsymptomaticArray[i], ModerateArray[i], SevereArray[i], DeadArray[i], ImmuneArray[i],helpme);
+	}
+	fclose(fp);
     ui->csvWidget->hide();
 }
